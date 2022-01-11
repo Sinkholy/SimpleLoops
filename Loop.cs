79c45ccf3edb296f5c8220e1d2839a65364a3486
Sinkholy <Sinkholy@gmail.com>
2022-01-11 21:50:06 +0300

    [Development][Базовая реализация for-цикла]

---
+++ Loop.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SimpleLoops
+{
+	public class Loop
+	{
+		public static ForLoop Repeat(int iterations)
+		{
+			return new ForLoop(iterations);
+		}
+
+		public class ForLoop
+		{
+			readonly int iterations;
+			int currentIteration;
+
+			public ForLoop(int iterations)
+			{
+				this.iterations = iterations;
+				currentIteration = 0;
+			}
+
+			public void Operation(Action<int> operation)
+			{
+				for (; currentIteration < iterations; currentIteration++)
+				{
+					operation(currentIteration);
+				}
+			}
+		}
+	}
+}
